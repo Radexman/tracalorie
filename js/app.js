7fd3150dd2656cc07d8a0c41885834f81de1fc83
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-06-17 19:30:17 +0200

    Tracker Stats

--- js/app.js
+++ js/app.js
@@ -4,16 +4,60 @@ class CalorieTracker {
 		this._totalCalories = 0;
 		this._meals = [];
 		this._workouts = [];
+
+		this._displayCaloriesLimit();
+		this._displayCaloriesTotal();
+		this._displayCaloriesConsumed();
+		this._displayCaloriesBurned();
+		this._displayCaloriesRemaining();
 	}
 
+	// Public Methods
 	addMeal(meal) {
 		this._meals.push(meal);
 		this._totalCalories += meal.calories;
+		this._render();
 	}
 
 	addWorkout(workout) {
 		this._workouts.push(workout);
 		this._totalCalories -= workout.calories;
+		this._render();
+	}
+
+	// Private Methods
+	_displayCaloriesTotal() {
+		const totalCaloriesEl = document.getElementById('calories-total');
+		totalCaloriesEl.textContent = this._totalCalories;
+	}
+	_displayCaloriesLimit() {
+		const calorieLimitEl = document.getElementById('calories-limit');
+		calorieLimitEl.textContent = this._calorieLimit;
+	}
+
+	_displayCaloriesConsumed() {
+		const caloriesConsumedEl = document.getElementById('calories-consumed');
+		const consumed = this._meals.reduce((acc, meal) => acc + meal.calories, 0);
+		caloriesConsumedEl.textContent = consumed;
+	}
+
+	_displayCaloriesBurned() {
+		const caloriesBurnedEl = document.getElementById('calories-burned');
+		const burned = this._workouts.reduce((acc, workout) => acc + workout.calories, 0);
+		caloriesBurnedEl.textContent = burned;
+	}
+
+	_displayCaloriesRemaining() {
+		const caloriesRemainingEl = document.getElementById('calories-remaining');
+		const remaining = this._calorieLimit - this._totalCalories;
+		caloriesRemainingEl.textContent = remaining;
+	}
+
+	_render() {
+		this._displayCaloriesTotal();
+		this._displayCaloriesConsumed();
+		this._displayCaloriesBurned();
+		this._displayCaloriesRemaining();
 	}
 }
 
@@ -38,8 +82,11 @@ const tracker = new CalorieTracker();
 const breakfast = new Meal('Breakfase', 400);
 tracker.addMeal(breakfast);
 
-const run = new Workout('Run', 300);
-tracker.addWorkout(run);
+const lunch = new Meal('Lunch', 720);
+tracker.addMeal(lunch);
 
-console.log(tracker._meals);
-console.log(tracker._workouts);
+const dinner = new Meal('Dinner', 1200);
+tracker.addMeal(dinner);
+
+const run = new Workout('Run', 320);
+tracker.addWorkout(run);
