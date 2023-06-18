aa2f3a6dc6f5738ff7740b1041f1565f2edea2d2
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-06-18 10:52:55 +0200

    Adding Workouts & Meals

--- js/app.js
+++ js/app.js
@@ -99,13 +99,62 @@ class Workout {
 	}
 }
 
-const tracker = new CalorieTracker();
+class App {
+	constructor() {
+		this._tracker = new CalorieTracker();
+		document.getElementById('meal-form').addEventListener('submit', this._newMeal.bind(this));
+		document.getElementById('workout-form').addEventListener('submit', this._newWorkout.bind(this));
+	}
 
-const breakfast = new Meal('Breakfase', 400);
-tracker.addMeal(breakfast);
+	_newMeal(e) {
+		e.preventDefault();
 
-const lunch = new Meal('Lunch', 500);
-tracker.addMeal(lunch);
+		const name = document.getElementById('meal-name');
+		const calories = document.getElementById('meal-calories');
 
-const run = new Workout('Run', 320);
-tracker.addWorkout(run);
+		// Validate inputs
+		if (name.value === '' || calories.value === '') {
+			alert('Please enter all fields');
+			return;
+		}
+
+		const meal = new Meal(name.value, +calories.value);
+
+		this._tracker.addMeal(meal);
+
+		name.value = '';
+		calories.value = '';
+
+		const collapseMeal = document.getElementById('collapse-meal');
+		const bsCollapse = new bootstrap.Collapse(collapseMeal, {
+			toggle: true,
+		});
+	}
+
+	_newWorkout(e) {
+		e.preventDefault();
+
+		const name = document.getElementById('workout-name');
+		const calories = document.getElementById('workout-calories');
+
+		// Validate inputs
+		if (name.value === '' || calories.value === '') {
+			alert('Please fill in all fields');
+			return;
+		}
+
+		const workout = new Workout(name.value, +calories.value);
+
+		this._tracker.addWorkout(workout);
+
+		name.value = '';
+		calories.value = '';
+
+		const collapseWorkout = document.getElementById('collapse-workout');
+		const bsCollapse = new bootstrap.Collapse(collapseWorkout, {
+			toggle: true,
+		});
+	}
+}
+
+const app = new App();
