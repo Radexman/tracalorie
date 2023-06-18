969e81c289df7077fc0a9293807547cc8586cd5c
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-06-18 12:20:04 +0200

    Remove Meal & Workout

--- js/app.js
+++ js/app.js
@@ -28,6 +28,28 @@ class CalorieTracker {
 		this._render();
 	}
 
+	removeMeal(id) {
+		const index = this._meals.findIndex((meal) => meal.id === id);
+
+		if (index !== -1) {
+			const meal = this._meals[index];
+			this._totalCalories -= meal.calories;
+			this._meals.splice(index, 1);
+			this._render();
+		}
+	}
+
+	removeWorkout(id) {
+		const index = this._workouts.findIndex((workout) => workout.id === id);
+
+		if (index !== -1) {
+			const workout = this._workouts[index];
+			this._totalCalories += workout.calories;
+			this._workouts.splice(index, 1);
+			this._render();
+		}
+	}
+
 	// Private Methods
 	_displayCaloriesTotal() {
 		const totalCaloriesEl = document.getElementById('calories-total');
@@ -103,13 +125,13 @@ class CalorieTracker {
 		workoutEl.setAttribute('data-id', workout.id);
 		workoutEl.innerHTML = `
 			<div class="card-body">
-			<div class="d-flex align-items-center justify-content-between">
-				<h4 class="mx-1">${workout.name}</h4>
-				<div class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5">${workout.calories}</div>
-				<button class="delete btn btn-danger btn-sm mx-2">
-					<i class="fa-solid fa-xmark"></i>
-				</button>
-			</div>
+				<div class="d-flex align-items-center justify-content-between">
+					<h4 class="mx-1">${workout.name}</h4>
+					<div class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5">${workout.calories}</div>
+					<button class="delete btn btn-danger btn-sm mx-2">
+						<i class="fa-solid fa-xmark"></i>
+					</button>
+				</div>
 			</div>
 		`;
 
@@ -146,6 +168,9 @@ class App {
 		this._tracker = new CalorieTracker();
 		document.getElementById('meal-form').addEventListener('submit', this._newItem.bind(this, 'meal'));
 		document.getElementById('workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'));
+
+		document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this, 'meal'));
+		document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'));
 	}
 
 	_newItem(type, e) {
@@ -175,6 +200,18 @@ class App {
 		const bsCollapse = new bootstrap.Collapse(collapseItem, {
 			toggle: true,
 		});
+	}
+
+	_removeItem(type, e) {
+		if (e.target.classList.contains('delete') || e.target.classList.contains('fa-xmark')) {
+			if (confirm('Are you sure')) {
+				const id = e.target.closest('.card').getAttribute('data-id');
+
+				type === 'meal' ? this._tracker.removeMeal(id) : this._tracker.removeWorkout(id);
+
+				e.target.closest('.card').remove();
+			}
+		}
 	}
 }
 
