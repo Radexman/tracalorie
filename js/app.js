40f36430663a86a7f34f88485d7cc40991ade659
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-06-19 12:31:45 +0200

    Clear localStorage

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ class CalorieTracker {
 		this._displayCaloriesBurned();
 		this._displayCaloriesRemaining();
 		this._displayCaloriesProgress();
+
+		document.getElementById('limit').value = this._calorieLimit;
 	}
 
 	// Public Methods
@@ -62,6 +64,7 @@ class CalorieTracker {
 		this._totalCalories = 0;
 		this._meals = [];
 		this._workouts = [];
+		Storage.clearAll();
 		this._render();
 	}
 
@@ -272,6 +275,12 @@ class Storage {
 		});
 
 		localStorage.setItem('workouts', JSON.stringify(workouts));
+	}
+
+	static clearAll() {
+		localStorage.removeItem('calorieTotal');
+		localStorage.removeItem('meals');
+		localStorage.removeItem('workouts');
 	}
 }
 
